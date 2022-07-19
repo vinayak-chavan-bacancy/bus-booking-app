13f30b99417364068a142f6e91ff6bfd721637a2
vinayak-chavan-bacancy <vinayak.chavan@bacancy.com>
2022-07-19 11:49:18 +0530

    add: login and register completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,17 +11,18 @@ const login = async (req, res) => {
 
     const emailID = req.body.emailID;
     const password = req.body.password;
-
+    console.log('body' + req.body);
     // check for email exist or not
     const userData = await user.findOne({ emailID: emailID });
     if (!userData) {
-      return errorResponse('Invalid credentials!', 404);
+      return errorResponse(req, res, 'Invalid credentials!', 404);
     }
-
+    console.log('userData' + userData);
     // check for the password
     const isMatch = await bcrypt.compare(password, userData.password);
+    console.log("isMatch" + isMatch);
     if (!isMatch) {
-      return errorResponse('Invalid credentials!', 404);
+      return errorResponse(req, res, 'Invalid credentials!', 404);
     } else {
 
       // jwt token created
@@ -29,20 +30,19 @@ const login = async (req, res) => {
         exp: 60 * 60,
         secret: process.env.ACCESS_TOKEN_SECRET,
       });
-
+      console.log(accessToken);
       await userData.save();
-      return res.status(200).send({ accessToken });
-
+      return successResponse(req, res, accessToken, 200);
     }
   } catch (error) {
-    return errorResponse('something went wrong!', 400, { err: error });
+    console.log(error.message)
+    return errorResponse(req, res, 'something went wrong!', 400, { err: error });
   }
 };
 
 const register = async (req, res) => {
   try {
-    const addinguserRecords = new user(req.body);
-    const emailID = req.body.emailID;
+    const { username, phoneno, emailID, password } = new user(req.body);
 
     // check if email id allready exist
     const userData = await user.findOne({ emailID: emailID });
@@ -51,14 +51,26 @@ const register = async (req, res) => {
       return errorResponse('email id allready exist', 400);
     } else {
 
+      // creating payload
+      const payload = {
+        username,
+        phoneno,
+        emailID,
+        password,
+        wallet: 0,
+        role: 'admin',
+      };
+
       // register new user
-      const insert = await addinguserRecords.save();
+      const newUser = new user(payload);
+      const insertUser = await newUser.save();
+
       console.log("Registration Successful");
 
-      return successResponse(req, res, insert, 200);
+      return successResponse(req, res, insertUser, 200);
     }
-  } catch (e) {
-    return errorResponse(req, res, "something went wrong", 400, { err: e });
+  } catch (error) {
+    return errorResponse(req, res, "something went wrong", 400 );
   }
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,23 @@
-const successResponse = (data, code = 200) => res.send({
-  code,
-  data,
-  success: true,
-});
+const successResponse = (req, res, data, code = 200) =>
+  res.send({
+    code,
+    data,
+    success: true,
+  });
 
 const errorResponse = (
-  errorMessage = 'Something went wrong',
+  req,
+  res,
+  errorMessage = "Something went wrong",
   code = 500,
-  error = {},
-) => res.status(code).json({
-  code,
-  errorMessage,
-  error,
-  data: null,
-  success: false,
-});
+  error = {}
+) =>
+  res.status(code).json({
+    code,
+    errorMessage,
+    error,
+    data: null,
+    success: false,
+  });
 
 module.exports = { successResponse, errorResponse };
