251628b2dd3ea4bdc0369aa44dc416e24def9bc1
vinayak-chavan-bacancy <vinayak.chavan@bacancy.com>
2022-07-19 09:58:27 +0530

    add: database connection created

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 (async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/todo-app?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false", {
+    await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
